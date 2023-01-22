19cb7687d6db70b9e73fd749d6f248be020763d2
nickjo24 <njohnso2@chicagobooth.edu>
2023-01-22 18:13:08 +0000

    Finished square and random forms and result pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,34 @@
 class ApplicationController < ActionController::Base
+  def homepage
+    render({ :template => "game_templates/rules.html.erb" })
+  end
 
+  def blank_square_form
+    render({ :template => "calc_templates/square_form.html.erb" })
+  end
+
+  def calculate_square
+
+    #Everything in the params hash is a string.
+
+    @num = params.fetch("number").to_f
+
+    @square = @num ** 2
+
+    render({ :template => "calc_templates/square_results.html.erb" })
+  end
+
+  def blank_random_form
+    render({ :template => "calc_templates/random_form.html.erb" })
+  end
+
+  def calculate_random
+
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+
+    @random = rand(@lower..@upper)
+
+    render({ :template => "calc_templates/rand_results.html.erb" })
+  end
 end
