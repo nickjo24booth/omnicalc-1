4c8b2e483813d221634f49cb5a8bd5381d9bb398
nickjo24 <njohnso2@chicagobooth.edu>
2023-01-22 19:36:53 +0000

    Got payment functionality working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,12 +23,44 @@ class ApplicationController < ActionController::Base
   end
 
   def calculate_random
-
     @lower = params.fetch("user_min").to_f
     @upper = params.fetch("user_max").to_f
 
     @random = rand(@lower..@upper)
 
     render({ :template => "calc_templates/rand_results.html.erb" })
+  end
+
+  def blank_sqrt_form
+    render({ :template => "calc_templates/sqrt_form.html.erb" })
+  end
+
+  def calculate_sqrt
+    @num = params.fetch("number").to_f
+
+    @sqrt = @num ** 0.5
+
+    render({ :template => "calc_templates/sqrt_results.html.erb" })
+  end
+
+  def blank_payment_form
+    render({ :template => "calc_templates/payment_form.html.erb" })
+  end
+
+  def calculate_pmt
+    @apr = params.fetch("user_apr").to_f
+    @r = (@apr / 100) / 12
+
+    @years = params.fetch("user_years").to_f
+    @n = @years * 12
+
+    @pv = params.fetch("user_pv").to_f
+
+    @numerator = @r * @pv
+    @denominator = 1 - ((1 + @r) ** (-@n))
+
+    @pmt = @numerator / @denominator
+
+    render({ :template => "calc_templates/payment_results.html.erb" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,12 @@ Rails.application.routes.draw do
   get("/random/new", { :controller => "application", :action => "blank_random_form" })
 
   get("/random/results", { :controller => "application", :action => "calculate_random" })
+
+  get("/square_root/new", { :controller => "application", :action => "blank_sqrt_form" })
+
+  get("/square_root/results", { :controller => "application", :action => "calculate_sqrt" })
+
+  get("/payment/new", { :controller => "application", :action => "blank_payment_form" })
+
+  get("/payment/results", { :controller => "application", :action => "calculate_pmt" })
 end
